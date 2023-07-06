9ed4f7dfeb9f00ea67c31175792dfa08db231de9
Ugo Chiori <ugochiori@gmail.com>
2023-07-06 13:21:37 +0100

    Healthtips

---
+++ src/Healthtips.tsx
@@ -0,0 +1,87 @@
+
+export const healthtips: {
+  title: string;
+}[] = [
+  {
+    title: "Wash your hands frequently",
+  },
+  {
+    title: "Maintain social distancing",
+  },
+  {
+    title: "Avoid touching your eyes, nose and mouth",
+  },
+  {
+    title: "Practice respiratory hygiene",
+  },
+  {
+    title: "If you have fever, cough and difficulty breathing, seek medical care early",
+  },
+  {
+    title: "Stay informed and follow advice given by your healthcare provider",
+    
+  },
+  {
+    title: "Women go for regular checkups with your gynecologist",
+  },
+  {
+    title: "Take your children for regular checkups with the pediatrician",
+  },
+  {
+    title: "Take your parents for regular checkups with the geriatrician",
+  },
+  {
+    title: "Check your blood pressure regularly",
+  },
+  {
+    title: "Check your blood sugar regularly",
+  },
+  {
+    title: "Check your cholesterol regularly",
+  },
+  {
+    title: "Check your weight regularly",
+  },
+  {
+    title: "Check your BMI regularly",
+  },
+  {
+    title: "Check your waist circumference regularly",
+  },
+  {
+    title: "Go for regular comprehensive checkups",
+  },
+  {
+    title: "Go for regular dental checkups",
+  },
+  {
+    title: "Go for regular eye checkups",
+  },
+  {
+    title: "Know your family health history",
+  },
+  {
+    title: "Know your personal health history",
+  },
+  {
+    title: "Know your allergies",
+  },
+  {
+    title: "Know your blood group",
+  },
+  {
+    title: "Know your medications",
+  },
+  {
+    title: "Know your vaccinations",
+  },
+  {
+    title: "Reduce your salt intake",
+  },
+  {
+    title: "Report your symptoms to your doctor",
+  },
+  {
+    title: "Get tested and treated for STIs",
+  }
+];
--- src/landingpage/HomePage.tsx
+++ src/landingpage/HomePage.tsx
@@ -4,43 +4,51 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { GiDoctorFace } from "react-icons/gi";
 import { useNavigate } from "react-router-dom";
 import { AiOutlinePlus } from "react-icons/ai";
+import { healthtips } from "../Healthtips";
 
 const HomePage: React.FC = () => {
+  const randomNum = Math.floor(Math.random() * Math.floor(healthtips.length));
+
   const navigate = useNavigate();
 
   return (
-    <div className="careheader_container">
-      <div className="careheader_wrapper">
-        <div className="careheader_head">
-          <h1 className="careheader_heading">
-            Search Your <span className="careheader_heading_span">Care</span>{" "}
-            Provider
-          </h1>
+    <div>
+      <div className="careheader_container">
+        <div className="careheader_wrapper">
+          <div className="careheader_head">
+            <h1 className="careheader_heading">
+              Search Your <span className="careheader_heading_span">Care</span>{" "}
+              Provider
+            </h1>
 
-          <div className="careheader_input">
-            <AiOutlineSearch className="careheader_input_icon" />
-            <input
-              type="text"
-              placeholder="Search"
-              className="careheader_input_element"
-              onClick={() => {
-                navigate("/register");
-              }}
-            />
+            <div className="careheader_input">
+              <AiOutlineSearch className="careheader_input_icon" />
+              <input
+                type="text"
+                placeholder="Search"
+                className="careheader_input_element"
+                onClick={() => {
+                  navigate("/register");
+                }}
+              />
+            </div>
+          </div>
+          <div className="careheader_select">
+            <div className="careheader_item">
+              <GiDoctorFace className="careheader_select_icon" />
+
+              <a href="/reviews">Reviews</a>
+            </div>
+            <div className="careheader_item">
+              <AiOutlinePlus className="careheader_select_icon" />
+
+              <a href="/addreview">Add Review</a>
+            </div>
           </div>
         </div>
-        <div className="careheader_select">
-          <div className="careheader_item">
-            <GiDoctorFace className="careheader_select_icon" />
-
-            <a href="/reviews">Reviews</a>
-          </div>
-          <div className="careheader_item">
-            <AiOutlinePlus className="careheader_select_icon" />
-
-            <a href="/addreview">Add Review</a>
-          </div>
-        </div>
+      </div>
+      <div className="careheader_healthtips">
+        <p>{healthtips[randomNum].title}</p>
       </div>
     </div>
   );
