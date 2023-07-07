1b08c187d79a858c15079398861aa8c13d09a8ec
Ugo Chiori <ugochiori@gmail.com>
2023-07-07 01:54:02 +0100

    admin

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import mdPlugin from 'vite-plugin-md';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mdPlugin from "vite-plugin-md";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-plugins: [
+  plugins: [
     react(),
     mdPlugin({
-      wrapperComponent: 'div',
+      wrapperComponent: "div",
       markdownItOptions: {
         html: true,
         linkify: true,
         typographer: true,
         breaks: true,
-        
       },
     }),
-],
-assetsInclude: ['**/*.md'],
+  ],
 
-})
+  assetsInclude: ["**/*.md"],
+ 
+});
+
+
